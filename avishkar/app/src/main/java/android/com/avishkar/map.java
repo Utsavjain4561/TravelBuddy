e18156955113269af4344d032045373099ee7f6a
Netfreak21 <aryankhanna.itsd@gmail.com>
2018-08-29 14:44:20 +0530

    second

---
+++ avishkar/app/src/main/java/android/com/avishkar/map.java
@@ -0,0 +1,44 @@
+package android.com.avishkar;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlacePicker;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class map extends AppCompatActivity{
+
+    int PLACE_PICKER_REQUEST = 1;
+    GoogleMap googleMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+        /*PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
+        try {
+            startActivityForResult(builder.build(this), PLACE_PICKER_REQUEST);
+        } catch (GooglePlayServicesRepairableException e) {
+            e.printStackTrace();
+        } catch (GooglePlayServicesNotAvailableException e) {
+            e.printStackTrace();
+        }*/
+
+    }
+
+
+
+
+    /*@Override
+    public void onMapReady(GoogleMap googleMap) {
+        this.googleMap = googleMap;
+    }*/
+}
